2beec7f5ec4a99014a666c18746772bf85b9d56d
Cute laughing meow <149303644+kadxm@users.noreply.github.com>
2024-08-25 17:43:44 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    // 添加动画效果
-    $('.navbar ul li a').hover(function() {
-        $(this).animate({ fontSize: '20px' }, 300);
-    }, function() {
-        $(this).animate({ fontSize: '16px' }, 300);
-    });
-});
